628cec40a373de9ec06e2921295a71d7aae83e5c
Arman0228 <erekarman@gmail.com>
2025-07-05 22:30:10 +0500

    Events, hover

--- playwright_events.py
+++ playwright_events.py
@@ -1,19 +1,27 @@
-from playwright.sync_api import sync_playwright, expect, Request, Response
+from playwright.sync_api import sync_playwright, Request, Response
 
+
+# Логирование запросов
 def log_request(request: Request):
-    print(f'Request: {request.url}')
+    print(f"Request: {request.url}")
 
+
+# Логирование ответов
 def log_response(response: Response):
-    print(f'Response: {response.url}, {response.status}')
+    print(f"Response: {response.url}")
 
 
 with sync_playwright() as playwright:
+    # Открываем браузер и создаём новую страницу
     browser = playwright.chromium.launch(headless=False)
     page = browser.new_page()
 
-    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
+    # Добавляем обработчики событий
+    page.on("request", log_request)  # Запрос отправлен
+    page.on("response", log_response)  # Ответ получен
 
-    page.on('request', log_request)
-    page.on('response', log_response)
+    # Переходим на страницу входа
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
-    page.wait_for_timeout(5000)
+    # Задержка для завершения всех запросов
+    page.wait_for_timeout(3000)
