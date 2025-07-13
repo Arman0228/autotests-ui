a0ab3c5f239550b7a460f2aca665c63292f02e42
Arman0228 <erekarman@gmail.com>
2025-07-13 20:23:51 +0500

    7.1 Знакомство с PageObject

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -1,19 +1,14 @@
+from playwright.sync_api import Page, expect
+
 from pages.base_page import BasePage
-from playwright.sync_api import expect
 
 
 class DashboardPage(BasePage):
-    def __init__(self, page):
+    def __init__(self, page: Page):
         super().__init__(page)
 
-        # Локатор заголовка Dashboard
         self.dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
 
-    def is_dashboard_title_visible(self):
-        """
-        Проверяет видимость заголовка Dashboard
-        :return: None (вызывает assertion)
-        """
+    def check_visible_dashboard_title(self):
         expect(self.dashboard_title).to_be_visible()
-        expect(self.dashboard_title).to_have_text("Dashboard")
-
+        expect(self.dashboard_title).to_have_text('Dashboard')
