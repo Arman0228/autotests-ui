dcf5ffce6bcf84ad86dc8ed5479337baa21734a5
Arman0228 <erekarman@gmail.com>
2025-07-14 22:31:47 +0500

    7.2 рактикуемся в реализации PageObject.практикуемся в написании автотестов с использованием POM

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,36 +1,100 @@
-from playwright.sync_api import expect, Page
+# from playwright.sync_api import expect, Page
+# import pytest
+#
+#
+# @pytest.mark.smoke
+# @pytest.mark.regression
+# def test_empty_courses_list(chromium_page_with_state: Page):
+#     """
+#     Тест проверяет, что:
+#     1. Отображается заголовок 'Courses'
+#     2. Видна иконка 'папка'
+#     3. Отображается текст 'There is no results'
+#     4. Отображается текст 'Results from the load test pipeline will be displayed here'
+#     """
+#     # Используем страницу с сохраненным состоянием из фикстуры
+#     page = chromium_page_with_state
+#
+#     # Переходим на страницу курсов
+#     page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+#
+#     # Проверяем наличие заголовка 'Courses'
+#     courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
+#     expect(courses_title).to_be_visible()
+#     expect(courses_title).to_have_text('Courses')
+#
+#     # Проверяем иконку 'папка'
+#     empty_icon = page.get_by_test_id('courses-list-empty-view-icon')
+#     expect(empty_icon).to_be_visible()
+#
+#     # Проверяем текст 'There is no results'
+#     empty_title = page.get_by_test_id('courses-list-empty-view-title-text')
+#     expect(empty_title).to_have_text('There is no results')
+#
+#     # Проверяем текст описания
+#     empty_description = page.get_by_test_id('courses-list-empty-view-description-text')
+#     expect(empty_description).to_have_text('Results from the load test pipeline will be displayed here')
+
+
 import pytest
+from playwright.sync_api import expect
+from pages.login_page import LoginPage
+from pages.courses_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
 
 
-@pytest.mark.smoke
+@pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list(chromium_page_with_state: Page):
-    """
-    Тест проверяет, что:
-    1. Отображается заголовок 'Courses'
-    2. Видна иконка 'папка'
-    3. Отображается текст 'There is no results'
-    4. Отображается текст 'Results from the load test pipeline will be displayed here'
-    """
-    # Используем страницу с сохраненным состоянием из фикстуры
-    page = chromium_page_with_state
+def test_create_course(login_page: LoginPage,
+                       create_course_page: CreateCoursePage,
+                       courses_list_page: CoursesListPage):
 
-    # Переходим на страницу курсов
-    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+    # 2. Переход на страницу создания курса
+    create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
 
-    # Проверяем наличие заголовка 'Courses'
-    courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
-    expect(courses_title).to_be_visible()
-    expect(courses_title).to_have_text('Courses')
+    # 3. Проверка элементов страницы
+    create_course_page.check_visible_create_course_title()
+    create_course_page.check_disabled_create_course_button()
+    create_course_page.check_visible_image_preview_empty_view()
+    create_course_page.check_visible_image_upload_view()
 
-    # Проверяем иконку 'папка'
-    empty_icon = page.get_by_test_id('courses-list-empty-view-icon')
-    expect(empty_icon).to_be_visible()
+    # 4. Проверка формы по умолчанию
+    create_course_page.check_visible_create_course_form(
+        title="",
+        estimated_time="",
+        description="",
+        max_score="0",
+        min_score="0"
+    )
 
-    # Проверяем текст 'There is no results'
-    empty_title = page.get_by_test_id('courses-list-empty-view-title-text')
-    expect(empty_title).to_have_text('There is no results')
+    # 5. Проверка раздела упражнений
+    create_course_page.check_visible_exercises_title()
+    create_course_page.check_visible_create_exercise_button()
+    create_course_page.check_visible_exercises_empty_view()
 
-    # Проверяем текст описания
-    empty_description = page.get_by_test_id('courses-list-empty-view-description-text')
-    expect(empty_description).to_have_text('Results from the load test pipeline will be displayed here')
+    # 6. Загрузка изображения
+    create_course_page.upload_preview_image("./testdata/files/image.png")
+    create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
+
+    # 7. Заполнение формы курса
+    create_course_page.fill_create_course_form(
+        title="Playwright",
+        estimated_time="2 weeks",
+        description="Playwright course description",
+        max_score="100",
+        min_score="10"
+    )
+
+    # 8. Создание курса
+    create_course_page.click_create_course_button()
+
+    # 9. Проверка результатов
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_course_card(
+        index=0,
+        title="Playwright",
+        max_score="100",
+        min_score="10",
+        estimated_time="2 weeks"
+    )
