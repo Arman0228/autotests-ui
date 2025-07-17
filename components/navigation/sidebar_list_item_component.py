362b234c256d2f4956260f444af8e194602c7977
Arman0228 <erekarman@gmail.com>
2025-07-17 23:25:46 +0500

    7.3 Знакомство с PageComponent.Практика работы с Page Component

--- components/navigation/sidebar_list_item_component.py
+++ components/navigation/sidebar_list_item_component.py
@@ -6,19 +6,18 @@ from components.base_component import BaseComponent
 
 
 class SidebarListItemComponent(BaseComponent):
-    def __init__(self, page: Page, identifier:str):
+    def __init__(self, page: Page, identifier: str):
         super().__init__(page)
 
         self.icon = page.get_by_test_id(f'{identifier}-drawer-list-item-icon')
         self.title = page.get_by_test_id(f'{identifier}-drawer-list-item-title-text')
-        self.icon = page.get_by_test_id(f'{identifier}-drawer-list-item-button')
-
+        self.button = page.get_by_test_id(f'{identifier}-drawer-list-item-button')
 
     def check_visible(self, title: str):
         expect(self.icon).to_be_visible()
 
         expect(self.title).to_be_visible()
-        expect(self.title).to_have_text()
+        expect(self.title).to_have_text(title)
 
         expect(self.button).to_be_visible()
 
