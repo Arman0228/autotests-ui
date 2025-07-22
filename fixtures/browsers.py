cbceb5f6c76759cec4dd431318133a973d7fc7fd
Arman0228 <erekarman@gmail.com>
2025-07-22 23:43:57 +0500

    7.7 Наращивание тестовой базы и рефакторинг

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -3,6 +3,8 @@ from typing import Any, Generator
 import pytest  # Импортируем pytest
 from playwright.sync_api import Page, Playwright
 
+from pages.authentication.registration_page import RegistrationPage
+
 
 @pytest.fixture  # Объявляем фикстуру, по умолчанию скоуп function, то что нам нужно
 def chromium_page(playwright: Playwright) -> Generator[Page, Any, None]:  # Аннотируем возвращаемое фикстурой значение
@@ -16,27 +18,14 @@ def initialize_browser_state(playwright: Playwright) -> Page:
     context = browser.new_context()
     page = context.new_page()
 
-    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-
-    registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-    registration_email_input.fill('username@gmail.com')
-
-    # Заполняем поле Username
-    registration_name_input = page.get_by_test_id('registration-form-username-input').locator('input')
-    registration_name_input.fill('username')
-
-    # Заполняем поле Password
-    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-    password_input.fill('password')
-
-    # Нажимаем на кнопку Registration
-    registration_button = page.get_by_test_id('registration-page-registration-button')
-    registration_button.click()
+    registration_page = RegistrationPage(page=page)
+    registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    registration_page.registration_form.fill(email='username@gmail.com', username='username', password='password')
+    registration_page.click_registration_button()
 
     # Сохранение состояния
     context.storage_state(path='browser-state.json')
     #Закрытие браузера
-    context.close()
     browser.close()
 
 
@@ -45,9 +34,7 @@ def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -
     """Фикстура для создания страницы с сохраненным состоянием браузера"""
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state="browser-state.json")
-    page = context.new_page()
-    yield page
+    yield context.new_page()
 
     # Очистка после теста
-    context.close()
     browser.close()
