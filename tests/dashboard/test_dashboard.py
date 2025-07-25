8e6d51b0e208697d103ccf6e8777a4fa320396aa
Arman0228 <erekarman@gmail.com>
2025-07-25 21:28:22 +0500

    8.3 Добавление Allure шагов в автотесты

--- tests/dashboard/test_dashboard.py
+++ tests/dashboard/test_dashboard.py
@@ -14,6 +14,9 @@ from tools.allure.srories import AllureStory
 @allure.epic(AllureEpic.LMS)
 @allure.feature(AllureFeature.DASHBOARD)
 @allure.story(AllureStory.DASHBOARD)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.DASHBOARD)
+@allure.sub_suite(AllureStory.DASHBOARD)
 class TestDashboard:
     @allure.title('Check displayning of dashboard page')
     @allure.severity(Severity.NORMAL)
